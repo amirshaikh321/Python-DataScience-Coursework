ccbf2eb64e60b188caff07b4c2e897d2493cdbea
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-19 09:35:16 +0530

    Functions Parameters

---
+++ day_03.py
@@ -0,0 +1,19 @@
+# string formating
+
+print('the kiran academy karvenagar pune')
+print('the kiran academy \nkarvenagar \npune')
+
+print('the \t\tkiran academy karvenagar pune')
+
+# predefine func parameters
+
+n1 = 1
+n2 = 2
+n3 = 3
+
+print(n1,n2,n3)
+print(n1,n2,n3, sep='-') # seperator
+
+print(n1, end='@')
+print(n2, end="@") # end parameter
+print(n3, end="@")
