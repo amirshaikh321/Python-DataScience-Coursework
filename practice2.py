7e29786eede047035cb18f17529003c7f2eed3f5
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-24 23:31:52 +0530

    Module imported

---
+++ practice2.py
@@ -0,0 +1,5 @@
+import practice1 # just import module
+from practice1 import Name # import only given 
+from practice1 import * # import all from module
+
+print(f"Name : {Name} | City : {practice1.City}")
