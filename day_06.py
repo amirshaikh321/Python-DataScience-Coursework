1ad4ec536a53614df343c128f14341b54ff56ccb
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-24 23:29:57 +0530

    Identifierrs

---
+++ day_06.py
@@ -0,0 +1,23 @@
+# Identtifiers in python
+
+# Identifiers are Alphanumeric
+num1 = 21 # contain Alphabets and numbers
+num2 = 22
+
+# Identifiers should not start with Number
+# 23name = "ram" -> raise error
+
+# Don't use space(' ') use '_' instead of space
+roll_no =21
+
+print('identifiers working')
+
+# Dont use keywords as identifiers
+# from =81
+# end = "jjsaf"
+
+# Keywords
+
+from keyword import kwlist
+
+print(kwlist)
