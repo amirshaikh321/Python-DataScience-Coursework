944c2b8075cf8854dc915f42bc398b93bc80c00e
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-24 23:29:07 +0530

    string format example

---
+++ day_05.py
@@ -0,0 +1,10 @@
+# WAP Movie ticket billing system
+
+Ticket_Price = 250
+No_tickets = 3
+
+print("Ticket price is {}".format('Ticket_Price'))
+print("No of tickets {}".format('No_tickets'))
+
+# create f string use table
+
