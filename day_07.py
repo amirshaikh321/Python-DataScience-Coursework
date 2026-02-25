70b39c59e79ac9d3ff421711131fd608395036b8
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-25 22:31:23 +0530

    Input function

---
+++ day_07.py
@@ -0,0 +1,22 @@
+# Input function
+
+# name = input('Enter Your name: ')
+# age = input('Enter Your Age: ')
+# course = input('Enter Your Course name: ')
+# print("\n","-"*50,"\n")
+# print(f"Name: {name} \nAge: {age}\nCourse: {course}")
+
+
+# WAP to square of any number
+
+num = int(input("Enter your number: "))
+square = num**2
+
+print(f"Suare of {num} is {square}")
+
+
+obtain_marks = int(input("Enter your marks: "))
+total_marks = 500
+percentage= (obtain_marks/total_marks)*100
+
+print(f"Percetage is {percentage}%")
