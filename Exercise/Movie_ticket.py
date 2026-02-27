8374e263a6b21a6946adc14e575b1c565376e461
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-27 23:50:30 +0530

    Movie ticket system

---
+++ Exercise/Movie_ticket.py
@@ -0,0 +1,29 @@
+# Task 3: Movie Ticket Billing System
+# A cinema hall wants to calculate total ticket cost.
+# Details:
+
+# Customer Name
+# Number of Tickets
+
+# Rules:
+
+# Cost per ticket = â‚¹250
+
+# Total Cost = Tickets Ã— Cost per ticket
+
+# Task:
+
+# Write a Python program to:
+
+# Accept customer name and ticket count
+
+# Calculate total cost
+
+# Display details using string formatting
+# -------------------------
+
+print("*"*7," Movie Ticket Booking System ","*"*7)
+cost  = 250
+
+print(f"\nCost of the ticket is {cost}","-"*5)
+Name = input("Enter your name:")
