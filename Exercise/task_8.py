5c46983381e1eb104ac51e0cd1bab9b85185213c
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-28 22:59:44 +0530

    Online Food Order bill

---
+++ Exercise/task_8.py
@@ -0,0 +1,28 @@
+# Task 8: Online Food Order Bill
+
+# A food app wants to generate bill.
+
+# Details:
+
+# Customer Name
+
+# Food Item Price
+
+# Rules:
+
+# Delivery Charge = â‚¹40
+
+# GST = 5% of Food Price
+
+# Total Bill = Price + GST + Delivery Charge
+
+# Task:
+
+# Write a Python program to:
+
+# Accept details
+
+# Calculate GST and total bill
+
+# Display bill using string formatting
+
