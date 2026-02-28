bdb6d4b2a45ebfd79908b31feefe5a7dfc5616b5
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-28 23:10:48 +0530

    online food Order Bill

--- Exercise/task_8.py
+++ Exercise/task_8.py
@@ -26,3 +26,11 @@
 
 # Display bill using string formatting
 
+print("*"*7," Online Food Order Bill ","*"*7)
+
+name = input("Enter your Name: ")
+recharge_amount = int(input("Recharge amount: "))
+delivery_charge = 40
+gst = recharge_amount*18/100
+Total_amount = recharge_amount + gst
+print(f"\n{name} you have to pay {Total_amount}")
