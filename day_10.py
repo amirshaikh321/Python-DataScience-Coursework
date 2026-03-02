54d28db496c13307ef3d012d61ed5de7a6baab78
Amir Shaikh <amirshaikh1594@gmail.com>
2026-03-02 20:19:54 +0530

    Numeric base form conversion

---
+++ day_10.py
@@ -0,0 +1,14 @@
+
+# decimal to binary conversion
+
+Num1 = 168732
+Num2 = bin(Num1) # This func returns how we can write the given num in the binary 
+print("-"*20)
+print("Number representation in Binary Format :",Num2,"\n")
+Num3 = 0b101001001100011100 # This store the decimal number but intialize in the binary format
+
+# Binary to decimal
+
+Num4 = int(Num3)
+print("-"*20)
+print("Number representation in Decimal Format :",Num4)
