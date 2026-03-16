c7d230cada4053adf35bdd42e80d962aa46a50a0
Amir Shaikh <amirshaikh1594@gmail.com>
2026-03-16 18:04:44 +0530

    Dictionary example: add marks in dictionary

---
+++ day_19.py
@@ -0,0 +1,25 @@
+
+marks = {'ajay pawar':{'t1':45,'t2':78},'vijay raut':{'t1':78,'t2':88}}
+
+print(marks['ajay pawar']['t2'])
+marks['vijay raut']['t3'] = 57
+marks['ajay pawar']['t3'] = 75
+print(marks)
+
+marks = {'ajay pawar': {'t1': 45, 't2': 78, 't3': 75}, 'vijay raut': {'t1': 78, 't2': 88, 't3': 57}}
+
+result = {}
+t1 = marks['ajay pawar']['t1']
+t2 = marks['ajay pawar']['t2']
+t3 = marks['ajay pawar']['t3']
+total = (t1+t2+t3)/3
+result['ajay pawar']= total
+
+t1 = marks['vijay raut']['t1']
+t2 = marks['vijay raut']['t2']
+t3 = marks['vijay raut']['t3']
+total = (t1+t2+t3)/3
+result['vijay raut']= total
+
+print(result)
+
