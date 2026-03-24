ef106e7e46ff9e80b6d2c773fe7b8bc81e2da610
Amir Shaikh <amirshaikh1594@gmail.com>
2026-03-24 21:31:07 +0530

    if else

---
+++ day_22.py
@@ -0,0 +1,22 @@
+"""
+
+if else statement
+
+"""
+
+age = int(input("Enter Your age : "))
+
+if age>=18:
+    print("You are eligible for voting")
+else:
+    print("You are not eligible for voting")
+
+
+num = int(input("Enter number:"))
+
+if num >0:
+    print(f"{num} is positive")
+elif num <0:
+    print(f"{num} is negative")
+else:
+    print("invalid number")
