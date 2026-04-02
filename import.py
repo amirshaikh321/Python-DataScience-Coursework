34098d5d3bd544c2b709b29cecb9f6bb8c2093e5
Amir Shaikh <amirshaikh1594@gmail.com>
2026-04-02 17:11:06 +0530

    Package import in python

---
+++ import.py
@@ -0,0 +1,12 @@
+import mymath
+# mymath.add()
+# mymath.add()
+# mymath.sub()
+
+mymath.square(88)
+mymath.iseven(45)
+
+mymath.div_5(5)
+mymath.si(50000,10,2)
+mymath.fullname('amir','shamshuddin','shaikh','male')
+mymath.email('amir','tcs')
