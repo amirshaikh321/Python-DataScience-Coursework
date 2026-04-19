4253ab368672af9b2f8dd498438f13369eb64a87
Amir Shaikh <amirshaikh1594@gmail.com>
2026-04-19 22:18:04 +0530

    Mock interview code

---
+++ que.py
@@ -0,0 +1,16 @@
+
+list = [12,41,63,12,575,63,12]
+l1 = []
+for i in list:
+    if i not in l1:
+        l1.append(i)
+print(l1)
+
+# num = int(input("Enter number:"))
+# if num %2==0:
+#     print(f"{num} Number is even")
+# else:
+#     print(f"{num} Number is odd")
+
+# l1 = set(list)
+# print(l1)
