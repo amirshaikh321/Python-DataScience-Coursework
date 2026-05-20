1559684391c18b70787c3841b3cb8b309d9f467c
Amir Shaikh <amirshaikh1594@gmail.com>
2026-05-20 18:23:35 +0530

    User Defined module

---
+++ advance_python/userdefinemodule.py
@@ -0,0 +1,22 @@
+
+# def fact(num1):
+#     fact = 1
+#     for i in range(1,num1+1):
+#         fact*= i
+#     return fact
+
+# def addition(a,b):
+#     return a + b\
+
+# def sub(a,b):
+#     return a - b
+
+# def div(a,b):
+#     return a / b
+
+# def mul(a,b):
+#     return a * b
+
+
+# pure func, closer, func composition,
+# how does immutability play arole in functional programming
