de710755ce9cd72708acaccfca97ca1764873b21
Amir Shaikh <amirshaikh1594@gmail.com>
2026-05-20 18:22:58 +0530

    Module import in python

---
+++ advance_python/module.py
@@ -0,0 +1,20 @@
+import math 
+print(math.factorial(5))
+
+import math as m
+print(m.sqrt(4))
+
+from math import pow
+print(pow(2,3))
+
+from math import *
+print(sqrt(10))
+
+from userdefinemodule import fact
+print(fact(5))
+
+import userdefinemodule as us
+print(us.addition(13,12))
+
+import calendar
+print(calendar.calendar(2026))
